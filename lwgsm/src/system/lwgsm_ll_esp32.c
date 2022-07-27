9eabb5b568f5878f653e81e6c03691d264080925
Ilya Kargapolov <d3vil.st@gmail.com>
2022-07-27 19:02:37 +0300

    Wake up modem before initiating

--- lwgsm/src/system/lwgsm_ll_esp32.c
+++ lwgsm/src/system/lwgsm_ll_esp32.c
@@ -135,6 +135,10 @@ configure_uart(uint32_t baudrate) {
     ESP_ERROR_CHECK(uart_param_config(GSM_UART_NUM, &uart_config));
     ESP_LOGD(TAG, "RX: %d TX: %d", *LWGSM_CFG_RX, *LWGSM_CFG_TX);
     ESP_ERROR_CHECK(uart_set_pin(GSM_UART_NUM, *LWGSM_CFG_TX, *LWGSM_CFG_RX, 0, 0));
+    ESP_ERROR_CHECK(uart_write_bytes(GSM_UART_NUM, 'AT\r\n', strlen('AT\r\n')));
+    lwgsm_delay(100);
+    ESP_ERROR_CHECK(uart_write_bytes(GSM_UART_NUM, 'AT\r\n', strlen('AT\r\n')));
+    lwgsm_delay(100);
 }
 /**
  * \brief           Callback function called from initialization process
